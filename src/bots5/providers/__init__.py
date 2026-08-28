627b80cde64a13c7480810f0caa9ed078d438b66
necromilias <38950966+necromilias@users.noreply.github.com>
2026-08-28 19:46:42 +1000

    Add B.O.T.S. 5 V0 implementation

---
+++ src/bots5/providers/__init__.py
@@ -0,0 +1,4 @@
+from .base import CompletionRequest, CompletionResult, Provider
+from .openrouter import OpenRouterProvider
+
+__all__ = ["CompletionRequest", "CompletionResult", "Provider", "OpenRouterProvider"]
